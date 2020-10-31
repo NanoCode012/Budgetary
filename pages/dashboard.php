c0e4696b011f189f27b315469d6803a0ecfba384
NanoCode012 <kevinvong@rocketmail.com>
2020-10-31 22:14:07 +0700

    Include ChartJS CDN and added test chart from php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,56 @@
 <!-- Placeholder Dashboard -->
+<div id="service-container" data-service="<?php
+                                                $data = [5, 4, 3];
+                                                echo json_encode($data);
+                                                ?>"></div>
+
+<div class="chart-container" style="position: relative; height:40vh; width:40vw">
+    <canvas id="myChart" width="100" height="100"></canvas>
+</div>
+
+
+<script>
+    window.onload = () => {
+        console.log(document.getElementById('service-container').getAttribute('data-service'));
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: document.getElementById('service-container').getAttribute('data-service'),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+</script>
+
+<!-- Charts.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
