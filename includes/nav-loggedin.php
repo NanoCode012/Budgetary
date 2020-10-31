0b1d0a3e477c7b16ac99df09a21992ad8139ca46
NanoCode012 <kevinvong@rocketmail.com>
2020-10-31 23:35:57 +0700

    Add css to navbar

--- includes/nav-loggedin.php
+++ includes/nav-loggedin.php
@@ -1,7 +1,21 @@
-<nav>
-<a href="?p=dashboard">Dashboard</a> |
-<a href="?p=expense">Expense</a> |
-<a href="?p=wallets">Wallets</a> |
-<a href="?p=settings">Settings</a> |
-<a href='?p=logout'>Logout</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="?p=dashboard">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?p=expense">Expense</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?p=wallets">Wallets</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?p=settings">Settings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href='?p=logout'>Logout</a>
+            </li>
+        </ul>
+    </div>
 </nav>
