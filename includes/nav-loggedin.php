1e4c7c28754a7df81299498be8e6da1e7de5004a
NanoCode012 <kevinvong@rocketmail.com>
2020-10-31 17:14:17 +0700

    Add logout to nav

--- includes/nav-loggedin.php
+++ includes/nav-loggedin.php
@@ -2,5 +2,6 @@
 <a href="?p=dashboard">Dashboard</a> |
 <a href="?p=expense">Expense</a> |
 <a href="?p=wallets">Wallets</a> |
-<a href="?p=settings">Settings</a>
+<a href="?p=settings">Settings</a> |
+<a href='?p=logout'>Logout</a>
 </nav>
