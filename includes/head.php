81770768494203ea5098b0a703e896bd9bc6f8a4
NanoCode012 <kevinvong@rocketmail.com>
2020-11-01 00:28:16 +0700

    Update JS script order

--- includes/head.php
+++ includes/head.php
@@ -17,5 +17,8 @@ include ('utils/messages.php');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <!-- <link rel="stylesheet" type="text/css" href="css/main.css"> -->
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <title><?php echo $servertitle; ?></title>
 </head>
