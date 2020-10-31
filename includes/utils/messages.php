0d1444ee31f110731dc59d0762c7a2119bcb121c
NanoCode012 <kevinvong@rocketmail.com>
2020-10-31 20:29:52 +0700

    Add expense feature (#7)

--- includes/utils/messages.php
+++ includes/utils/messages.php
@@ -19,4 +19,19 @@ $m_email = 'Email';
 $m_currency = 'Currency';
 $m_login = 'Login';
 $m_register = 'Register';
+$m_title = 'Title';
+$m_category = 'Category';
+$m_amount = 'Amount';
+$m_wallet = 'Wallet';
+$m_description = 'Description';
+
+// Expenses
+$m_add = 'Add';
+$m_emptytitle = 'No title provided';
+$m_emptycategory = 'No category provided';
+$m_emptyamount = 'No amount provided';
+$m_emptywallet = 'No wallet provided';
+$m_emptydescription = 'No description provided';
+$m_addsuccess = 'Added';
+$m_adderror = 'Fail to add';
 ?>
