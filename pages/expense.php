9942c3b82c744603c64274e2dd24f6a26b31bfec
NanoCode012 <kevinvong@rocketmail.com>
2020-11-15 00:30:13 +0700

    Fixed title expense

--- pages/expense.php
+++ pages/expense.php
@@ -84,9 +84,9 @@ if (isset($msgBox)) echo $msgBox;
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                        <div class="card-header">
+                        <!-- <div class="card-header">
                             <h4 class="card-title"> Expense</h4>
-                        </div>
+                        </div> -->
                         <div class="card-body">
                             <div class="table-responsive">
                                 <div class="d-flex flex-row-reverse"><button type="button" class="btn btn-outline-primary" 
