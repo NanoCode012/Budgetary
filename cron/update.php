f24b08f297799f570b7a4ef21a90faa37c1b55b4
NanoCode012 <kevinvong@rocketmail.com>
2020-11-17 01:29:00 +0700

    Update time modified for currency

--- cron/update.php
+++ cron/update.php
@@ -4,24 +4,25 @@ include '../config/dbconf.php';
 
 $rows = $db->run('SELECT * FROM currency;');
 
-$arr = array();
+$arr = [];
 foreach ($rows as $row) {
     $arr[] = $row['name'];
 }
 
-$s = join(",",$arr);
+$s = join(',', $arr);
 
 $json = file_get_contents(
-    'http://api.currencylayer.com/live?access_key=bef8fb3a7e2b87c2928cd7c6508ef983&currencies=' . $s . '&format=1'
+    'http://api.currencylayer.com/live?access_key=bef8fb3a7e2b87c2928cd7c6508ef983&currencies=' .
+        $s .
+        '&format=1'
 );
 
-$obj = json_decode($json,true);
+$obj = json_decode($json, true);
 $quotes = $obj['quotes'];
 echo json_encode($quotes) . '<br>';
 
-
 foreach ($rows as $row) {
-    $r = 1/$quotes['USD'.$row['name']];
+    $r = 1 / $quotes['USD' . $row['name']];
     echo 'USD -> ' . $row['name'] . ' = ' . $r . '<br>';
     $db->update(
         'currency',
