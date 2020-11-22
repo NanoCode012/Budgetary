9bd814db4eec1d2635c8056d3ad2db1c005788d3
NanoCode012 <kevinvong@rocketmail.com>
2020-11-22 23:10:12 +0700

    Remove unnecessary comments

--- includes/foot.php
+++ includes/foot.php
@@ -5,18 +5,6 @@
 <!-- <script src="./assets/js/plugins/perfect-scrollbar.jquery.min.js" type="text/javascript"></script> -->
 <!-- <script src="../assets/js/plugins/moment.min.js"></script> -->
 
-<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-<!-- <script src="../assets/js/plugins/bootstrap-switch.js"></script> -->
-
-<!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
-<!-- <script src="../assets/js/plugins/jquery.dataTables.min.js"></script> -->
-
- <!-- Google Maps Plugin   
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
-
-<!-- Chart JS -->
-<!-- <script src="./assets/js/plugins/chartjs.min.js"></script> -->
-
 <!--  Notifications Plugin    -->
 <script src="./assets/js/plugins/bootstrap-notify.js"></script>
 
